111c143651edae0b8026bc1d1868459ee17786e6
Andre Luis <aistiger.98@gmail.com>
2019-09-26 18:55:30 -0300

    configuracao inicial do servidor

---
+++ index.js
@@ -0,0 +1,26 @@
+let app = require('express')();
+let server = require('http').createServer(app);
+let io = require('socket.io')(server);
+
+var port = process.env.PORT || 3001;
+
+io.on('connection', (socket) => {
+    console.log(socket);
+
+    socket.on('disconnect', function() {
+        io.emit('users-changed', { user: socket.username, event: 'left' });
+    });
+
+    socket.on('set-name', (name) => {
+        socket.username = name;
+        io.emit('user-changed', {user: name, event: 'joined'});
+    });
+
+    socket.on('send-message', (message) => {
+        io.emit('message', {msg: message.text, user: socket.username, createdAt: new Date()});
+    });
+});
+
+server.listen(port, function() {
+    console.log('listen on port: http://localhost:' + port);
+});
