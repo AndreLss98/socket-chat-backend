ebf4101f6b04fd644fbaaba3104b037277e03170
Andre Luis <aistiger.98@gmail.com>
2019-09-29 14:31:47 -0300

    individualizando mensagens do chat e guardando usuarios na memoria

--- index.js
+++ index.js
@@ -4,18 +4,33 @@ let io = require('socket.io')(server);
 
 var port = process.env.PORT || 3001;
 
+let users = [];
+
 io.on('connection', (socket) => {
+
     socket.on('disconnect', function() {
-        io.emit('users-changed', { user: socket.username, event: 'left' });
+        let pos = users.findIndex(element => element.id === socket.id);
+        if (pos > -1) {
+            users.splice(pos, 1);
+        }
+        io.emit('users-changed', users);
     });
 
     socket.on('set-name', (name) => {
         socket.username = name;
-        io.emit('users-changed', {user: name, event: 'joined'});
+        users.push({
+            id: socket.id,
+            userName: name
+        });
+        io.emit('users-changed', users);
     });
 
-    socket.on('send-message', (message) => {
+    socket.on('send-message-all-users', (message) => {
         io.emit('message', {msg: message.text, user: socket.username, createdAt: new Date()});
+    });
+
+    socket.on('private-message', (data) => {
+        socket.broadcast.to(data.id).emit('send-private-message', {message: data.message, user: data.userName});
     });
 });
 
